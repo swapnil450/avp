66e42a86d062d0687c4371bea375eaca0be22483
hari <harischetan717@gmail.com>
2023-08-25 15:02:45 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
-
+// tailwind.config.js
+const { nextui } = require("@nextui-org/react");
 module.exports = {
-    publicRuntimeConfig: {
-      SERVER: process.env.NEXT_PUBLIC_SERVER_NAME,
-     
-    }
-  };
-  
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+  publicRuntimeConfig: {
+    SERVER: process.env.NEXT_PUBLIC_SERVER_NAME,
+  },
+};
